848e361ce79838ee8a59b81163d223cb2893c92b
NarwhalTaco <yu.ivan0352@gmail.com>
2023-11-23 20:24:36 -0500

    Add my files

---
+++ Student.java
@@ -0,0 +1,28 @@
+public class Student {
+    private int studentID, age;
+    private String name;
+    private double GPA;
+
+    public Student(int sID, int age, String name, double GPA) {
+        this.studentID = sID;
+        this.age = age;
+        this.name = name;
+        this.GPA = GPA;
+    }
+
+    public int getStuID()   {return this.studentID;}
+    public int getAge()     {return this.age;}
+    public String getName() {return this.name;}
+    public double getGPA()  {return this.GPA;}
+
+    public String toString() {
+        String tmp = "";
+        tmp += "****************";
+        tmp += "Student ID: " + getStuID();
+        tmp += "Name: " + getName();
+        tmp += "Age: " + getAge();
+        tmp += "GPA: " + getGPA();
+        tmp += "****************";
+        return tmp;
+    }
+}
